99a06172cff24c5e2371a7f0cb5deac7bcb83237
Goran Ninkovic <ninkovic.goran@itonomy.nl>
2021-07-11 23:48:16 +0200

    fix: remove angled brackes in from the project_name prefix This is to ensure docker-compose config compatibility

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 if [ "$2" ]; then
-    git clone https://github.com/TerrorSquad/docker-magento-quick-install.git $1 && cd $1
+    git clone https://github.com/TerrorSquad/docker-magento-quick-install.git "$1" && cd "$1"
 fi
 
 git config --global core.autocrlf false
@@ -8,6 +8,6 @@ git config --global core.eol LF
 git config --global core.fileMode false
 git config --global diff.renamelimit 5000
 
-sed -i docker-compose.yml -e "s/<project_name>/$1/g"
+sed -i docker-compose.yml -e "s/project_name/$1/g"
 cp env/default.conf.sample env/default.conf &&
     cp env/nginx.conf.sample env/nginx.conf
