b8f0e023ba848ed58c3b8fca6b229e0fb637a55b
Goran Ninkovic <ninkovic.goran@itonomy.nl>
2021-07-25 15:40:24 +0200

    fix: nginx init copy still needed because of docker mounts

--- init.sh
+++ init.sh
@@ -11,5 +11,5 @@ git config --global core.fileMode false
 git config --global diff.renamelimit 5000
 
 sed -i docker-compose.yml -e "s/project_name/$1/g"
-# cp config/nginx/default.conf.sample config/nginx/default.conf &&
-#     cp config/nginx/nginx.conf.sample config/nginx/nginx.conf
+cp config/nginx/default.conf.sample config/nginx/default.conf &&
+    cp config/nginx/nginx.conf.sample config/nginx/nginx.conf
