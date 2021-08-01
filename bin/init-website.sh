7911f62caa915a020becf0e020314a4fc3b3d69f
Goran Ninkovic <g.ninkovic@beeit.rs>
2021-08-01 02:01:10 +0200

    feat: init all base urls based on available stores

--- bin/init-website.sh
+++ bin/init-website.sh
@@ -3,3 +3,5 @@
 # shellcheck source=../env/db.env
 source env/db.env
 bin/mysql <config/sql/set_default_website.sql
+bin/generate-base-urls-sql-setter >config/sql/base_urls_setter.sql
+bin/mysql <config/sql/base_urls_setter.sql
