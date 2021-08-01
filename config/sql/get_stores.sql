63d31c273d98ce68786d9ec370e2aacf1c3bfed3
Goran Ninkovic <g.ninkovic@beeit.rs>
2021-08-01 12:19:35 +0200

    chore: generate mappings only for active stores

--- config/sql/get_stores.sql
+++ config/sql/get_stores.sql
@@ -1,4 +1,13 @@
 SELECT JSON_ARRAYAGG(
-        JSON_OBJECT('name', name, 'code', code, 'store_id', store_id)
+        JSON_OBJECT(
+            'name',
+            name,
+            'code',
+            code,
+            'store_id',
+            store_id,
+            'is_active',
+            is_active
+        )
     )
 from magento.store;
