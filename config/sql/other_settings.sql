80992c2afd685eddbb6b556d28ba2b46168b7e7a
Goran Ninkovic <change@me.com>
2021-09-20 22:47:40 +0200

    feat: add script that sets cookie and https config settings

--- config/sql/other_settings.sql
+++ config/sql/other_settings.sql
@@ -1,3 +1,9 @@
 USE magento;
 REPLACE INTO core_config_data (scope_id, path, value)
-VALUES (0, 'web/secure/use_in_adminhtml', 1)
+VALUES (0, 'web/secure/use_in_adminhtml', 1);
+REPLACE INTO core_config_data (scope_id, path, value)
+VALUES (0, 'web/cookie/cookie_domain', 'magento.test');
+REPLACE INTO core_config_data (scope_id, path, value)
+VALUES (0, 'web/secure/use_in_frontend', 1);
+REPLACE INTO core_config_data (scope_id, path, value)
+VALUES (0, 'web/secure/use_in_adminhtml', 1);
