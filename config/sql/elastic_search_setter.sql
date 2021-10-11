8187f56048ded676b5a14881ed61920ac6b63128
Goran Ninkovic <change@me.com>
2021-10-11 21:43:47 +0200

    feat: set vsbridge elastic search default settings

--- config/sql/elastic_search_setter.sql
+++ config/sql/elastic_search_setter.sql
@@ -1,7 +1,9 @@
 USE magento;
+-- Standard ElasticSearch settings
 UPDATE core_config_data ccd
 SET ccd.value = 'elasticsearch'
 WHERE ccd.path = 'catalog/search/elasticsearch7_server_hostname';
+-- Smile Elastic Suite settings
 UPDATE core_config_data ccd
 SET ccd.value = 'elasticsearch:9200'
 WHERE ccd.path = 'smile_elasticsuite_core_base_settings/es_client/servers';
@@ -29,3 +31,10 @@ WHERE ccd.path = 'smile_elasticsuite_core_base_settings/indices_settings/number_
 UPDATE core_config_data ccd
 SET ccd.value = 0
 WHERE ccd.path = 'smile_elasticsuite_core_base_settings/indices_settings/number_of_replicas';
+-- VSBridge settings
+UPDATE core_config_data ccd
+SET ccd.value = 'elasticsearch'
+WHERE ccd.path = 'vsbridge_indexer_settings/es_client/host';
+UPDATE core_config_data ccd
+SET ccd.value = '9200'
+WHERE ccd.path = 'vsbridge_indexer_settings/es_client/port';
