2e2bec5cc0de5ad345d44c30209849dd6aa8ca01
Goran Ninkovic <g.ninkovic@beeit.rs>
2021-11-17 14:02:20 +0100

    temp: use gsed with sed

--- init.sh
+++ init.sh
@@ -10,6 +10,6 @@ git config --global core.eol LF
 git config --global core.fileMode false
 git config --global diff.renamelimit 5000
 
-sed -i docker-compose.yml -e "s/project_name/$1/g"
+gsed -i docker-compose.yml -e "s/project_name/$1/g"
 cp config/nginx/default.conf.sample config/nginx/default.conf &&
     cp config/nginx/nginx.conf.sample config/nginx/nginx.conf
