10ebbc15d8226fc45e0e4505596788a4b17dc6de
Goran Ninkovic <g.ninkovic@beeit.rs>
2021-11-30 13:29:11 +0100

    fix: don't change global git config

--- init.sh
+++ init.sh
@@ -5,11 +5,6 @@ if [ "$2" ]; then
     git clone https://github.com/TerrorSquad/docker-magento-quick-install.git "$1" && cd "$1"
 fi
 
-git config --global core.autocrlf false
-git config --global core.eol LF
-git config --global core.fileMode false
-git config --global diff.renamelimit 5000
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
     sed -i '' -e "s/project_name/$1/g" docker-compose.yml
 else
