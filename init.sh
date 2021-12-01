6d9befeeb807bd7f7b5b21da952fb77b77957bb0
Goran Ninkovic <g.ninkovic@beeit.rs>
2021-12-01 14:13:12 +0100

    fix: make init.sh posix compliant

--- init.sh
+++ init.sh
@@ -5,10 +5,8 @@ if [ "$2" ]; then
     git clone https://github.com/TerrorSquad/docker-magento-quick-install.git "$1" && cd "$1"
 fi
 
-if [[ "$OSTYPE" == "darwin"* ]]; then
-    sed -i '' -e "s/project_name/$1/g" docker-compose.yml
-else
-    sed -i -e "s/project_name/$1/g" docker-compose.yml
-fi
+sed -e "s/project_name/$1/g" docker-compose.yml >docker-compose.yml.new &&
+    mv -f docker-compose.yml.new docker-compose.yml
+
 cp config/nginx/default.conf.sample config/nginx/default.conf &&
     cp config/nginx/nginx.conf.sample config/nginx/nginx.conf
