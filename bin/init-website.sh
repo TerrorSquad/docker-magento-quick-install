a28ab9088b36800023f48aec22dcb4940fac70ca
Goran Ninkovic <goran@ninkovic.tech>
2022-04-21 13:54:00 +0200

    chore: set cookies for each store

--- bin/init-website.sh
+++ bin/init-website.sh
@@ -3,8 +3,8 @@
 # shellcheck source=../env/db.env
 source env/db.env
 bin/mysql <config/sql/set_default_website.sql
-bin/generate-base-urls-sql-setter >config/sql/base_urls_setter.sql
-bin/mysql <config/sql/base_urls_setter.sql
+bin/generate-store-setters >config/sql/store_setters.sql
+bin/mysql <config/sql/store_setters.sql
 bin/update-nginx-config
 
 bin/mysql <config/sql/elastic_search_setter.sql
