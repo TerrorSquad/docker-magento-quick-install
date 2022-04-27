081970253a1ca8a162f2c4b16a3f17e07392fe95
Goran Ninkovic <goran@ninkovic.tech>
2022-04-27 21:26:20 +0200

    chore: remove project-name prefix from docker-compose

--- init.sh
+++ init.sh
@@ -5,8 +5,5 @@ if [ "$2" ]; then
     git clone https://github.com/TerrorSquad/docker-magento-quick-install.git "$1" && cd "$1"
 fi
 
-sed -e "s/project_name/$1/g" docker-compose.yml >docker-compose.yml.new &&
-    mv -f docker-compose.yml.new docker-compose.yml
-
 cp config/nginx/default.conf.sample config/nginx/default.conf &&
     cp config/nginx/nginx.conf.sample config/nginx/nginx.conf
