5abe143acd5c20b8a0188953bc744bc8bd64e462
Goran Ninkovic <g.ninkovic@youweagency.com>
2022-12-02 12:57:05 +0100

    feat: add setup-domain-multistore script

--- bin/init-website.sh
+++ bin/init-website.sh
@@ -2,10 +2,6 @@
 
 # shellcheck source=../env/db.env
 source env/db.env
-bin/mysql <config/sql/set_default_website.sql
-bin/generate-store-setters >config/sql/store_setters.sql
-bin/mysql <config/sql/store_setters.sql
-bin/update-nginx-config
-
+bin/setup-domain-multistore
 bin/mysql <config/sql/elastic_search_setter.sql
 bin/mysql <config/sql/other_settings.sql
