1e3cd26045c83449fcb58dbedfaed7dd5776e732
Goran Ninkovic <g.ninkovic@youweagency.com>
2022-12-18 13:31:58 +0100

    chore: exclude personal admin account from 2fa check

--- config/sql/other_settings.sql
+++ config/sql/other_settings.sql
@@ -1,9 +1,29 @@
 USE magento;
+
 REPLACE INTO core_config_data (scope_id, path, value)
-VALUES (0, 'web/secure/use_in_adminhtml', 1);
+VALUES
+    (0, 'web/secure/use_in_adminhtml', 1);
+
 REPLACE INTO core_config_data (scope_id, path, value)
-VALUES (0, 'web/cookie/cookie_domain', 'magento.test');
+VALUES
+    (0, 'web/cookie/cookie_domain', 'magento.test');
+
 REPLACE INTO core_config_data (scope_id, path, value)
-VALUES (0, 'web/secure/use_in_frontend', 1);
+VALUES
+    (0, 'web/secure/use_in_frontend', 1);
+
 REPLACE INTO core_config_data (scope_id, path, value)
-VALUES (0, 'web/secure/use_in_adminhtml', 1);
+VALUES
+    (0, 'web/secure/use_in_adminhtml', 1);
+
+REPLACE INTO core_config_data (scope_id, path, value)
+VALUES
+    (0, 'admin/security/session_lifetime', 31536000);
+
+REPLACE INTO core_config_data (scope_id, path, value)
+VALUES
+    (
+        0,
+        'twofactorauth/general/disabled_users',
+        'g.ninkovic@youweagency.com'
+    );
